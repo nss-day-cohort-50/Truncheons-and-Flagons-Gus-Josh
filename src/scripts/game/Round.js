33f68548e9253a48ec5f1eaf64e9e1dfed34a66f
Josh Pulley <josh.pulley@icloud.com>
2021-08-17 13:05:22 -0500

    app will now walk through 3 rounds of game then reset

--- src/scripts/game/Round.js
+++ src/scripts/game/Round.js
@@ -1,11 +1,54 @@
-import { getApplicationData } from "../dataAccess.js";
-
+import { getApplicationData, setObject, setTransientHTML } from "../dataAccess.js";
+const gameContainer = document.querySelector("#containerTwo")
 export const Rounds = (num)=>{
-    const teams = getApplicationData("transientState")
-    return `<h1>Round ${num}</h1>
-            ${teams.map(team =>{
+    const selectedTeams = getApplicationData("transientState")
+    const allTeams = getApplicationData("teams")
+    
+    const roundOneTwo = `<h1>Round ${num}</h1>
+            ${selectedTeams.map(team =>{
+                const foundTeam = allTeams.find((foundTeam)=>{
+                    return foundTeam.id === team.teamId})
                 return `
-                <label for="teamOne">${team.name}</label>
-                <input name="teamOne"type="number" id="teamscore--${team.id}">`}).join("")}
+                <label for="team${team.teamId}">${foundTeam.name}</label><br>
+                <input name="team${team.teamId}"type="number" id="teamscore--${team.teamId}"><br><br>`}).join("")}
+            <button id="nextRoundButton">Next Round</button>
             `
+    const lastRound = `<h1>Final Round</h1>
+                        ${selectedTeams.map(team => {
+                            const foundTeam = allTeams.find((foundTeam) => {
+                                return foundTeam.id === team.teamId
+                            })
+                            return `
+                            <label for="team${team.teamId}">${foundTeam.name}</label><br>
+                            <input name="team${team.teamId}"type="number" id="teamscore--${team.teamId}"><br><br>`
+                        }).join("")}
+                        <button id="finalRound">Finish Game</button>`
+    if (num <3){
+        return roundOneTwo
+    }else{
+        return lastRound
+    }
 }
+export const ScoreAdder = ()=>{
+    const selectedTeams = getApplicationData("transientState")
+    const selectedTeamsScore = selectedTeams.map((team)=>{
+        const roundScore = gameContainer.querySelector(`#teamscore--${team.teamId}`).value
+        team.score += parseInt(roundScore)
+        return team
+    })
+    selectedTeamsScore.map((team)=>{
+        setObject(team)
+    })
+    
+
+}
+gameContainer.addEventListener("click",
+    (event)=>{
+        if (event.target.id === "nextRoundButton"){
+            ScoreAdder()
+            setTransientHTML()
+        }else if (event.target.id === "finalRound"){
+            ScoreAdder()
+            setTransientHTML()
+        }
+    })
